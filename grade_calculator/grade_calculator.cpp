da80f06a43060eab44d6a46d64f4d27aab5c55bf
Iris <irisliu203@gmail.com>
2018-06-06 23:03:08 -0700

    Yeah, connected all the spin boxes and sliders.

--- grade_calculator/grade_calculator.cpp
+++ grade_calculator/grade_calculator.cpp
@@ -8,6 +8,53 @@ grade_calculator::grade_calculator(QWidget *parent) :
     ui->setupUi(this);
     QObject::connect(ui->spinBox,SIGNAL(valueChanged(int)),
                      this, SLOT(update_overall(int)));
+
+    //Connect spinBox to the horizontalSlider
+    QObject::connect(ui->spinBox,SIGNAL(valueChanged(int)),
+                     ui->horizontalSlider, SLOT(setValue(int)));
+    QObject::connect(ui->spinBox_2,SIGNAL(valueChanged(int)),
+                     ui->horizontalSlider_2, SLOT(setValue(int)));
+    QObject::connect(ui->spinBox_3,SIGNAL(valueChanged(int)),
+                     ui->horizontalSlider_3, SLOT(setValue(int)));
+    QObject::connect(ui->spinBox_4,SIGNAL(valueChanged(int)),
+                     ui->horizontalSlider_4, SLOT(setValue(int)));
+    QObject::connect(ui->spinBox_5,SIGNAL(valueChanged(int)),
+                     ui->horizontalSlider_5, SLOT(setValue(int)));
+    QObject::connect(ui->spinBox_6,SIGNAL(valueChanged(int)),
+                     ui->horizontalSlider_6, SLOT(setValue(int)));
+    QObject::connect(ui->spinBox_7,SIGNAL(valueChanged(int)),
+                     ui->horizontalSlider_7, SLOT(setValue(int)));
+    QObject::connect(ui->spinBox_8,SIGNAL(valueChanged(int)),
+                     ui->horizontalSlider_8, SLOT(setValue(int)));
+    QObject::connect(ui->spinBox_9,SIGNAL(valueChanged(int)),
+                     ui->horizontalSlider_9, SLOT(setValue(int)));
+    QObject::connect(ui->spinBox_10,SIGNAL(valueChanged(int)),
+                     ui->horizontalSlider_10, SLOT(setValue(int)));
+    QObject::connect(ui->spinBox_11,SIGNAL(valueChanged(int)),
+                     ui->horizontalSlider_11, SLOT(setValue(int)));
+    //The other way around.
+    QObject::connect(ui->horizontalSlider,SIGNAL(valueChanged(int)),
+                     ui->spinBox, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_2,SIGNAL(valueChanged(int)),
+                     ui->spinBox_2, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_3,SIGNAL(valueChanged(int)),
+                     ui->spinBox_3, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_4,SIGNAL(valueChanged(int)),
+                     ui->spinBox_4, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_5,SIGNAL(valueChanged(int)),
+                     ui->spinBox_5, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_6,SIGNAL(valueChanged(int)),
+                     ui->spinBox_6, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_7,SIGNAL(valueChanged(int)),
+                     ui->spinBox_7, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_8,SIGNAL(valueChanged(int)),
+                     ui->spinBox_8, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_9,SIGNAL(valueChanged(int)),
+                     ui->spinBox_9, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_10,SIGNAL(valueChanged(int)),
+                     ui->spinBox_10, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_11,SIGNAL(valueChanged(int)),
+                     ui->spinBox_11, SLOT(setValue(int)));
 }
 
 grade_calculator::~grade_calculator()
