27661ff166e420988ab799bcbfb630d22174006d
Iris <irisliu203@gmail.com>
2018-06-07 20:40:10 -0700

    Now the radio buttons are linked. Some minor problems to fix. Almost done.

--- grade_calculator/grade_calculator.cpp
+++ grade_calculator/grade_calculator.cpp
@@ -6,6 +6,10 @@ grade_calculator::grade_calculator(QWidget *parent) :
     ui(new Ui::grade_calculator)
 {
     ui->setupUi(this);
+    ui->radioButton->setChecked(true);
+    QObject::connect(ui->radioButton_2,SIGNAL(clicked(bool)),
+                     this, SLOT(update_overall(int)));
+
     //Update overall based on signal of each spinbox
     QObject::connect(ui->spinBox,SIGNAL(valueChanged(int)),
                      this, SLOT(update_overall(int)));
@@ -76,6 +80,8 @@ grade_calculator::grade_calculator(QWidget *parent) :
                      ui->spinBox_10, SLOT(setValue(int)));
     QObject::connect(ui->horizontalSlider_11,SIGNAL(valueChanged(int)),
                      ui->spinBox_11, SLOT(setValue(int)));
+
+    emit compute_overall();
 }
 
 grade_calculator::~grade_calculator()
@@ -93,8 +99,22 @@ void grade_calculator::update_overall(int unused){
     double h6 = ui->spinBox_6->value();
     double h7 = ui->spinBox_7->value();
     double h8 = ui->spinBox_8->value();
-    score = (h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8) * 0.25;
-
+    double m1 = ui->spinBox_9->value();
+    double m2= ui->spinBox_10->value();
+    double final = ui->spinBox_11->value();
+    score = (h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8)/8.0 * 0.25;
+    if(ui->radioButton->isChecked()){
+        score = score + (m1+m2)*0.2;
+        score = score + final*0.35;
+    }
+    if(ui->radioButton_2->isChecked()){
+        if(m1>=m2){
+            score = score + m1*0.3;
+        }else{
+            score = score + m2*0.3;
+        }
+        score += final*0.44;
+    }
     ui->label_14->setText(QString::number(score));
     return;
 }
